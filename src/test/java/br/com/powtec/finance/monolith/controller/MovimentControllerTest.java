e3a55eb2a9fc113122b7847ca34411355de0c988
Eduardo Arruda <eduardo.a3@icloud.com>
2024-05-15 00:02:20 -0300

    ## Search Moviments Feature
    - Created unit tests;
    - Added arguments for searching moviments

--- src/test/java/br/com/powtec/finance/monolith/controller/MovimentControllerTest.java
+++ src/test/java/br/com/powtec/finance/monolith/controller/MovimentControllerTest.java
@@ -1,21 +1,36 @@
 package br.com.powtec.finance.monolith.controller;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.web.servlet.MockMvc;;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest
+import br.com.powtec.finance.monolith.service.MovimentService;
+
+@WebMvcTest(MovimentController.class)
 public class MovimentControllerTest {
 
   @Autowired
   private MockMvc restCaller;
 
+  @MockBean
+  private MovimentService service;
+
+  @Test // Adjust test to validate required arguments
+  public void should_returnBadRequest_when_requiredArgumentsAreMissing() throws Exception {
+    // when(service.search()).thenReturn(null);
+    this.restCaller.perform(get("/moviments:search")).andExpect(status().isBadRequest());
+    this.restCaller.perform(get("/moviments:search?_limit=0")).andExpect(status().isBadRequest());
+    this.restCaller.perform(get("/moviments:search?_offset=0")).andExpect(status().isBadRequest());
+  }
+
   @Test
-  public void getMovimentsByPage() throws Exception {
-    this.restCaller.perform(get("/moviments:search")).andExpect(status().isOk());
+  public void should_returnSuccess_when_requiredArgumentsAreFilled() throws Exception {
+    this.restCaller.perform(get("/moviments:search?_offset=0&_limit=0")).andExpect(status().isOk());
   }
 }
