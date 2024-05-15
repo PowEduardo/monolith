9c1e3184f30f57a35bf9a39c2e7d2da0a852db3c
Eduardo Arruda <eduardo.a3@icloud.com>
2024-05-15 00:02:20 -0300

    ## Search Moviments Feature
    - Created unit tests;
    - Added arguments for searching moviments

--- src/main/java/br/com/powtec/finance/monolith/controller/MovimentController.java
+++ src/main/java/br/com/powtec/finance/monolith/controller/MovimentController.java
@@ -3,6 +3,7 @@ package br.com.powtec.finance.monolith.controller;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.powtec.finance.monolith.model.dto.MovimentDTO;
@@ -11,7 +12,8 @@ import br.com.powtec.finance.monolith.model.dto.MovimentDTO;
 public class MovimentController {
 
   @GetMapping("/moviments:search")
-  public ResponseEntity<Page<MovimentDTO>> searchMoviments() {
+  public ResponseEntity<Page<MovimentDTO>> searchMoviments(@RequestParam(value = "_limit") Integer limitPerPage,
+      @RequestParam(value = "_offset") Integer pageNumber) {
     return ResponseEntity.ok().build();
   }
 }
---
+++ src/main/java/br/com/powtec/finance/monolith/service/MovimentService.java
@@ -0,0 +1,10 @@
+package br.com.powtec.finance.monolith.service;
+
+import org.springframework.data.domain.Page;
+
+import br.com.powtec.finance.monolith.model.dto.MovimentDTO;
+
+public interface MovimentService {
+
+  public Page<MovimentDTO> search();
+}
