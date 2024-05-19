a7cb9e08156cd396db9beec37ea20fa74fe01b57
Eduardo Arruda <eduardo.a3@icloud.com>
2024-05-18 22:16:48 -0300

    ## Refactor
    - Validation Includded on path parameters
    - Tests organized

---
+++ src/main/java/br/com/powtec/finance/monolith/constants/ValidationMessagesConstants.java
@@ -0,0 +1,6 @@
+package br.com.powtec.finance.monolith.constants;
+
+public class ValidationMessagesConstants {
+  public static final String MINIMUM_ELEMENTS_PER_PAGE = "Minimum value for elements is 1";
+  public static final String MINIMUM_PAGE_NUMBER = "Minimum value for return a page is 0";
+}
