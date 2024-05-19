7c38a58650946a3a90c2b6ffbb06bcce24d3b003
Eduardo Arruda <eduardo.a3@icloud.com>
2024-05-18 22:00:17 -0300

    ## Test Refactored

--- src/test/java/br/com/powtec/finance/monolith/service/MovimentServiceImplTest.java
+++ src/test/java/br/com/powtec/finance/monolith/service/MovimentServiceImplTest.java
@@ -1,21 +1,36 @@
 package br.com.powtec.finance.monolith.service;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ActiveProfiles;
 
+import br.com.powtec.finance.monolith.mapper.MovimentMapper;
+import br.com.powtec.finance.monolith.model.MovimentModel;
 import br.com.powtec.finance.monolith.model.dto.MovimentDTO;
+import br.com.powtec.finance.monolith.repository.MovimentRepository;
 
 @SpringBootTest
 @ActiveProfiles(profiles = "test")
+@SuppressWarnings("unchecked")
 public class MovimentServiceImplTest {
 
   @Autowired
   private MovimentService service;
+
+  @MockBean
+  private MovimentMapper mapper;
+
+  @MockBean
+  MovimentRepository repository;
 
   @Test
   public void should_instantiateService_when_springIsUp() {
@@ -24,7 +39,12 @@ public class MovimentServiceImplTest {
 
   @Test
   public void should_returnPage_when_serviceCalled() {
+    Page<MovimentModel> pageModel = mock(Page.class);
+    Page<MovimentDTO> pageResponse = mock(Page.class);
+    when(repository.findAll(any(Pageable.class))).thenReturn(pageModel);
+    when(mapper.toPageDto(pageModel)).thenReturn(pageResponse);
     Page<MovimentDTO> page = service.search(0, 15);
     assertNotNull(page);
   }
+
 }
