ea7bea38aa19de33e1570922d3f84902675a94d8
Eduardo Arruda <eduardo.a3@icloud.com>
2024-07-02 06:32:41 -0300

    Refatoração

--- src/main/java/br/com/powtec/finance/monolith/model/dto/AssetDTO.java
+++ src/main/java/br/com/powtec/finance/monolith/model/dto/AssetDTO.java
@@ -3,6 +3,7 @@ package br.com.powtec.finance.monolith.model.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
+import br.com.powtec.finance.monolith.enums.AssetTypeEnum;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,5 +21,5 @@ public class AssetDTO {
   private Long id;
   private String ticker;
   private Double value;
-
+  private AssetTypeEnum type;
 }
