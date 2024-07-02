5b40134870a8cbb9d3166a3caa698598ffc673de
Eduardo Arruda <eduardo.a3@icloud.com>
2024-07-02 06:32:41 -0300

    Refatoração

--- src/main/java/br/com/powtec/finance/monolith/mapper/impl/AssetMapperImpl.java
+++ src/main/java/br/com/powtec/finance/monolith/mapper/impl/AssetMapperImpl.java
@@ -43,6 +43,7 @@ public class AssetMapperImpl implements AssetMapper {
         .id(dto.getId())
         .ticker(dto.getTicker())
         .value(dto.getValue())
+        .type(dto.getType())
         .build();
   }
 
