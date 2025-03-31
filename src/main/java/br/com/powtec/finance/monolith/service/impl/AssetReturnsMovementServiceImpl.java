1b9daf860701ba6d0418b40fc315348701db058b
Eduardo <eduardo.silva@icloud.com>
2025-03-31 08:04:03 -0300

    Refatoracao

--- src/main/java/br/com/powtec/finance/monolith/service/impl/AssetReturnsMovementServiceImpl.java
+++ src/main/java/br/com/powtec/finance/monolith/service/impl/AssetReturnsMovementServiceImpl.java
@@ -1,14 +1,17 @@
 package br.com.powtec.finance.monolith.service.impl;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import br.com.powtec.finance.database.library.enums.AssetReturnsOperationEnum;
 import br.com.powtec.finance.database.library.mapper.MovementMapper;
 import br.com.powtec.finance.database.library.model.dto.AssetReturnsMovementDTO;
 import br.com.powtec.finance.database.library.model.movement.AssetReturnsMovementModel;
 import br.com.powtec.finance.database.library.repository.AssetRepository;
+import br.com.powtec.finance.database.library.repository.AssetReturnsMovementRepository;
 import br.com.powtec.finance.database.library.repository.MovementRepository;
 import br.com.powtec.finance.database.library.repository.specification.BaseCrudMovementSpecification;
 
@@ -20,7 +23,6 @@ public class AssetReturnsMovementServiceImpl
       @Autowired MovementMapper<AssetReturnsMovementModel, AssetReturnsMovementDTO> mapper,
       @Autowired @Qualifier("assetReturnsMovementSpecification") BaseCrudMovementSpecification<AssetReturnsMovementModel> specification) {
     super(repository, mapper, specification);
-    this.repository.findAll(Sort.by(Sort.Direction.DESC, "exDividendDate"));
   }
 
   @Autowired
@@ -28,8 +30,15 @@ public class AssetReturnsMovementServiceImpl
 
   @Override
   public AssetReturnsMovementDTO create(AssetReturnsMovementDTO request, Long assetId) {
+    if (request.getAmount() == null) {
+      request.setAmount(getAmount(assetId, request.getExDividendDate()));
+    }
     if (request.getValue() == null) {
       calcReturnValue(request);
+      if (request.getIrFee() == null && request.getOperation() == AssetReturnsOperationEnum.JCP) {
+        request.setValue(request.getUnitValue() * request.getAmount() * 0.85);
+        request.setIrFee(request.getUnitValue() * request.getAmount() * 0.15);
+      }
     }
     request
         .setDescription(request.getOperation().toString() + " " + assetRepository.findById(assetId).get().getTicker());
@@ -38,5 +47,10 @@ public class AssetReturnsMovementServiceImpl
 
   private void calcReturnValue(AssetReturnsMovementDTO dto) {
     dto.setValue(dto.getUnitValue() * dto.getAmount());
+  }
+
+  private Integer getAmount(Long assetId, LocalDate date) {
+    AssetReturnsMovementRepository repository = (AssetReturnsMovementRepository) this.repository;
+    return repository.calcAmount(assetId, date);
   }
 }
