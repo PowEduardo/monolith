0bfd67ce8d258dfecfb1d24798b2a76cfb7ca700
Eduardo Arruda <eduardo.a3@icloud.com>
2025-08-26 07:28:57 -0300

    Run docker shell

--- docker-run-database.sh
+++ docker-run-database.sh
@@ -1,3 +1,3 @@
-docker run --name postgres --network finance-network -p 5432:5432 -v pgdata:/var/lib/postgresql/data -e POSTGRES_PASSWORD=123456 -e POSTGRES_USER=finance_user -e POSTGRES_DB=finance -d postgres 
-docker run --name my-pgadmin -e PGADMIN_DEFAULT_EMAIL=admin@admin.com -e PGADMIN_DEFAULT_PASSWORD=admin -p 8080:80 -d dpage/pgadmin4
-docker run --name monolith --network finance-network -p 9000:8080 monolith
+docker run --name postgres --network finance-network -p 5432:5432 -m 1g -v pgdata:/var/lib/postgresql/data -e POSTGRES_PASSWORD=123456 -e POSTGRES_USER=finance_user -e POSTGRES_DB=finance -d postgres 
+docker run --name pgadmin -e PGADMIN_DEFAULT_EMAIL=admin@admin.com -e PGADMIN_DEFAULT_PASSWORD=admin -p 8000:80 -m 1g -d dpage/pgadmin4:latest
+docker run --name monolith --network finance-network -p 9000:8080 -m 1g -d monolith
