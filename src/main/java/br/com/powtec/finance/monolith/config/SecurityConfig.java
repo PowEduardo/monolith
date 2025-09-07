3365ec0b1b3761da80e5314d69104a1bfe1f6acd
Eduardo <eduardo.silva@icloud.com>
2025-09-07 13:46:10 -0300

    Config security

--- src/main/java/br/com/powtec/finance/monolith/config/SecurityConfig.java
+++ src/main/java/br/com/powtec/finance/monolith/config/SecurityConfig.java
@@ -12,7 +12,10 @@ public class SecurityConfig {
     return new WebMvcConfigurer() {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*");
+        registry.addMapping("/**")
+          .allowedOrigins("*")
+          .allowedMethods("*")
+          .allowedHeaders("*");
       }
     };
   }
